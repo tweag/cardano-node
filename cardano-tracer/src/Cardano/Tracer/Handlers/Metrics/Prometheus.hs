eb7cb2fccf811ee1cd04cd0795196cf2dfd95f4c
Michael Karg <michael.karg@iohk.io>
2025-11-17 16:23:52 +0100

    cardano-tracer: Prometheus HTTP service discovery

--- cardano-tracer/src/Cardano/Tracer/Handlers/Metrics/Prometheus.hs
+++ cardano-tracer/src/Cardano/Tracer/Handlers/Metrics/Prometheus.hs
@@ -14,15 +14,19 @@ import           Cardano.Tracer.MetaTrace
 
 import           Prelude hiding (head)
 
+import           Control.Monad (join)
+import           Data.Aeson (ToJSON (..), encode, pairs, (.=))
 import qualified Data.ByteString as ByteString
-import           Data.ByteString.Builder (stringUtf8)
 import           Data.Functor ((<&>))
-import           Data.Text (Text)
+import qualified Data.Map as Map (Map, empty)
+import           Data.Maybe
+import           Data.Text as T (Text)
+import qualified Data.Text.Encoding as T (decodeUtf8)
 import qualified Data.Text.Lazy as TL
 import           Data.Text.Lazy.Builder (Builder)
 import qualified Data.Text.Lazy.Encoding as TL
 import           Network.HTTP.Types
-import           Network.Wai hiding (responseHeaders)
+import           Network.Wai
 import           Network.Wai.Handler.Warp (defaultSettings, runSettings)
 import           System.Metrics as EKG (Store, sampleAll)
 import           System.Time.Extra (sleep)
@@ -30,7 +34,7 @@ import           System.Time.Extra (sleep)
 -- | Runs a simple HTTP server that listens on @endpoint@.
 --
 --   At the root, it lists the connected nodes, either as HTML or JSON, depending
---   on the requests 'Accept: ' header.
+--   on the request's 'Accept: ' header.
 --
 --   Routing is dynamic, depending on the connected nodes. A valid URL is derived
 --   from the nodeName configured for the connecting node. E.g. a node name
@@ -40,10 +44,6 @@ import           System.Time.Extra (sleep)
 -- # TYPE Mem_resident_int gauge
 -- # HELP Mem_resident_int Kernel-reported RSS (resident set size)
 -- Mem_resident_int 103792640
--- # TYPE rts_gc_max_bytes_used gauge
--- rts_gc_max_bytes_used 5811512
--- # TYPE rts_gc_gc_cpu_ms counter
--- rts_gc_gc_cpu_ms 50
 -- # TYPE RTS_gcMajorNum_int gauge
 -- # HELP RTS_gcMajorNum_int Major GCs
 -- RTS_gcMajorNum_int 4
@@ -56,6 +56,31 @@ import           System.Time.Extra (sleep)
 -- # TYPE nodeCannotForge_int gauge
 -- # HELP nodeCannotForge_int How many times was this node unable to forge [a block]?
 --
+--  The `/targets` path can be used for Prometheus HTTP service discovery. This lets
+--  Prometheus dynamically discover all connected nodes, and scrape their metrics.
+--  Below is a minimal example of a corresponding job definition that goes into the
+--  `prometheus.yml` configuration:
+--
+--    - job_name: "cardano-tracer"
+--
+--      metrics_path: '/targets'
+--
+--      http_sd_configs:
+--      - url: 'http://127.0.0.1:3200/targets'  # <-- Your cardano-tracer's real hostname:prometheus port
+--
+--      relabel_configs:
+--      - source_labels: [__address__]
+--        target_label: __param_target
+--      - source_labels: [__param_target]
+--        target_label: instance
+--      - target_label: __address__
+--        replacement: 127.0.0.1:3200           # <-- Your cardano-tracer's real hostname:prometheus port
+--
+--  In cardano-tracer's config, you can optionally provide labels that will be attached to all discovered
+--  targets (default is no labels):
+--    "prometheusLabels": {
+--        "<labelname>": "<labelvalue>", ...
+--      }
 runPrometheusServer
   :: TracerEnv
   -> Endpoint
@@ -71,31 +96,26 @@ runPrometheusServer tracerEnv endpoint computeRoutes_autoUpdate = do
     { ttPrometheusEndpoint = endpoint
     }
   runSettings (setEndpoint endpoint defaultSettings) do
-    renderPrometheus computeRoutes_autoUpdate noSuffix teMetricsHelp
+    renderPrometheus computeRoutes_autoUpdate noSuffix teMetricsHelp promLabels
   where
     TracerEnv
       { teTracer
-      , teConfig = TracerConfig { metricsNoSuffix }
+      , teConfig = TracerConfig { metricsNoSuffix, prometheusLabels }
       , teMetricsHelp
       } = tracerEnv
 
-    noSuffix = or @Maybe metricsNoSuffix
+    noSuffix    = or @Maybe metricsNoSuffix
+    promLabels  = fromMaybe Map.empty prometheusLabels
 
 renderPrometheus
   :: IO RouteDictionary
   -> Bool
   -> [(Text, Builder)]
+  -> Map.Map Text Text
   -> Application
-renderPrometheus computeRoutes_autoUpdate noSuffix helpTextDict request send = do
+renderPrometheus computeRoutes_autoUpdate noSuffix helpTextDict promLabels request send = do
   routeDictionary :: RouteDictionary <-
     computeRoutes_autoUpdate
-
-  let acceptHeader :: Maybe ByteString.ByteString
-      acceptHeader = lookup hAccept $ requestHeaders request
-
-  let wantsJson, wantsOpenMetrics :: Bool
-      wantsJson         = all @Maybe ("application/json"             `ByteString.isInfixOf`) acceptHeader
-      wantsOpenMetrics  = all @Maybe ("application/openmetrics-text" `ByteString.isInfixOf`) acceptHeader
 
   case pathInfo request of
 
@@ -104,17 +124,51 @@ renderPrometheus computeRoutes_autoUpdate noSuffix helpTextDict request send = d
         then (contentHdrJSON    , renderJson routeDictionary)
         else (contentHdrUtf8Html, renderListOfConnectedNodes "Prometheus metrics" routeDictionary)
 
-    route:_
-      | Just (store :: EKG.Store, _) <- lookup route (getRouteDictionary routeDictionary)
-     -> do metrics <- getMetricsFromNode noSuffix helpTextDict store
-           send $ responseBuilder status200
-            (if wantsOpenMetrics then contentHdrOpenMetrics else contentHdrUtf8Text)
-            (TL.encodeUtf8Builder metrics)
+    ["targets"]
+      | wantsJson, isNothing targetQueryParam
+      -> serviceDiscovery routeDictionary
+
+      | Just target <- targetQueryParam
+      -> case lookup target (getRouteDictionary routeDictionary) of
+          Just (store :: EKG.Store, _) -> metricsExposition store
+          Nothing                      -> notFound target
 
       | otherwise
-     -> send $ responseBuilder status404 contentHdrUtf8Text do
-        "Not found: "
-          <> stringUtf8 (show route)
+      -> badRequest
+
+    route:_
+      | Just (store :: EKG.Store, _) <- lookup route (getRouteDictionary routeDictionary)
+     -> metricsExposition store
+
+      | otherwise
+     -> notFound route
+
+  where
+    acceptHeader :: Maybe ByteString.ByteString
+    acceptHeader = lookup hAccept $ requestHeaders request
+
+    wantsJson, wantsOpenMetrics :: Bool
+    wantsJson         = all @Maybe ("application/json"             `ByteString.isInfixOf`) acceptHeader
+    wantsOpenMetrics  = all @Maybe ("application/openmetrics-text" `ByteString.isInfixOf`) acceptHeader
+
+    targetQueryParam :: Maybe Text
+    targetQueryParam = fmap T.decodeUtf8 $ join $ lookup "target" (queryString request)
+
+    metricsExposition store = do
+      metrics <- getMetricsFromNode noSuffix helpTextDict store
+      send $ responseBuilder status200
+        (if wantsOpenMetrics then contentHdrOpenMetrics else contentHdrPrometheus)
+        (TL.encodeUtf8Builder metrics)
+
+    serviceDiscovery (RouteDictionary routeDict) =
+      let slugs = map fst routeDict
+      in send $ responseLBS status200 contentHdrJSON $
+        encode [PSD (slugs, promLabels) | (not . null) slugs]
+
+    notFound t = send $ responseLBS status404 contentHdrUtf8Text $
+      "Not found: " <> (TL.encodeUtf8 . TL.fromStrict) t
+    badRequest = send $ responseLBS status400 contentHdrUtf8Text
+      "Bad Request"
 
 getMetricsFromNode
   :: Bool
@@ -123,3 +177,13 @@ getMetricsFromNode
   -> IO TL.Text
 getMetricsFromNode noSuffix helpTextDict ekgStore =
   sampleAll ekgStore <&> renderExpositionFromSampleWith helpTextDict noSuffix
+
+
+newtype PrometheusServiceDiscovery = PSD ([Text], Map.Map Text Text)
+
+instance ToJSON PrometheusServiceDiscovery where
+  toJSON _ = error "ToJSON.toJSON(PrometheusServiceDiscovery): implementation error"
+
+  toEncoding (PSD (slugs, labelMap)) = pairs $
+       ("targets" .= slugs)
+    <> ("labels"  .= labelMap)
