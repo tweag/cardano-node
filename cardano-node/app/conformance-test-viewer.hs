1e2ead902a6cc021585f737bef1d9c3a73479773
Nathan Bloomfield <nathan.bloomfield@tweag.io>
2025-12-15 18:20:09 -0600

    Use optparse-applicative

--- cardano-node/app/conformance-test-viewer.hs
+++ cardano-node/app/conformance-test-viewer.hs
@@ -4,24 +4,24 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
--- {-# OPTIONS_GHC -Wno-missing-signatures -Wno-unused-top-binds -Wno-unused-imports #-}
-
 module Main (main) where
 
 import           Prelude hiding (lookup)
 
-import           Control.Monad ((>=>))
+import           Control.Monad (when)
 import           Data.Aeson (eitherDecode, encode, FromJSON, ToJSON)
 import           Data.Aeson.Encode.Pretty (encodePretty)
 import qualified Data.ByteString.Lazy as BS
 import           Data.Char (isDigit)
+import           Data.Function (on)
 import           Data.Proxy
 import           Data.List (groupBy)
+import qualified ExitCodes as Exit
+import           Options.Applicative
 import           ShrinkIndex
-import           System.Console.GetOpt
 import           System.Environment (getArgs)
 import           System.Exit
-import           System.IO (hPutStr, hPutChar, stderr)
+import           System.IO (hPutStr, hPutStrLn, hPutChar, stderr)
 import           Test.QuickCheck (Arbitrary)
 import           Text.Read (readEither)
 
@@ -29,19 +29,16 @@ import           Text.Read (readEither)
 
 main :: IO ()
 main = do
-  opts <- getOptions
+  opts <- getArgs >>= getOptions
 
-  if optShowHelp opts
-    then showUsage >> exitSuccess
-    else pure ()
-    
-  if optShowVersion opts
-    then showVersion >> exitSuccess
-    else pure ()
+  when (optShowVersion opts) $
+    showVersion >> Exit.exitWithStatus Exit.Success
   
-  if optDebug opts
-    then analyzeTreeOf (Proxy :: Proxy Int) opts
-    else analyzeTreeOf (Proxy :: Proxy Int) opts -- For now just a dummy implementation
+  case optDebug opts of
+    True  -> analyzeTreeOf (Proxy :: Proxy Int) opts
+    False -> analyzeTreeOf (Proxy :: Proxy Int) opts -- For now just a dummy implementation
+
+
 
 -- Analyze the shrink tree of a value of any type that
 -- implements `Arbitrary`, `FromJSON`, and `ToJSON`.
@@ -80,102 +77,70 @@ analyzeTreeOf _ opts = do
 
 
 
-getOptions :: IO Options
-getOptions = do
-  argv <- getArgs
-
-  let (actions, params, errs) = getOpt Permute options argv
-  
-  if null errs
-    then pure ()
-    else do
-      errPutStrLn "option error(s):"
-      mapM_ errPutStrLn errs
-      showUsage >> exitFailure
-    
-  -- Options as parsed only from the flags.
-  opts <- case compose actions defaultOpts of
-    Right ok -> pure ok
-    Left err -> do
-      errPutStrLn $ "Option parsing error: " <> err
-      showUsage >> exitFailure
-
-  -- Adjust to account for the input path being passed as
-  -- an argument instead of a flag.
-  case params of
-    [] -> pure opts
-    [inputPath] -> pure $ opts { optInputPath = Just inputPath }
-    _ -> do
-      errPutStrLn $ "unrecognized arguments: " <> show params
-      showUsage >> exitFailure
+getOptions :: [String] -> IO Options
+getOptions args = do
+  case execParserPure defaultPrefs options args of
+    Success opts -> pure opts
+    Failure failure -> do
+      let (msg, _) = renderFailure failure nameString
+      hPutStrLn stderr msg
+      Exit.exitWithStatus Exit.BadUsage
+    CompletionInvoked compl -> do
+      msg <- execCompletion compl nameString
+      putStr msg
+      Exit.exitWithStatus Exit.Success
 
 
-
--- This is Kleisli `mconcat`.
-compose :: (Monad m) => [a -> m a] -> a -> m a
-compose = foldr (>=>) return
 
 data Mode
   = ShowDescendant
   deriving (Eq, Show)
 
+options :: ParserInfo Options
+options = info (infoOption "INFO" (long "help" <> short '?') <*> optionParser) $ mconcat
+  [ fullDesc
+  ]
+
+optionParser :: Parser Options
+optionParser = Options
+  <$> flag False True
+    (long "version")
+  <*> (option auto
+    (long "input" <> mconcat
+        [ metavar "FILE_PATH"
+        , value Nothing
+        , help "File path for the test case file (JSON)"
+        ]))
+  <*> (option (eitherReader parseShrinkIndexOption)
+    (long "shrink-index" <> mconcat
+        [ short 'i'
+        , metavar "SHRINK_INDEX"
+        , value (path [])
+        , help "An index pointing to a shrunken test case"
+        ]))
+  <*> (option auto
+    (long "output" <> mconcat
+        [ metavar "FILE_PATH"
+        , value Nothing
+        , help "File path writing output"
+        ]))
+  <*> flag True False
+    (long "notpretty")
+  <*> flag False True
+    (long "debug")
+  <*> pure ShowDescendant
+
 data Options = Options
-  { optShowHelp    :: Bool           -- ^ Show usage?
-  , optShowVersion :: Bool           -- ^ Show version info?
+  { optShowVersion :: Bool           -- ^ Show version info?
   , optInputPath   :: Maybe FilePath
   , optShrinkIndex :: ShrinkIndex
   , optOutputPath  :: Maybe FilePath
-  , optPrettyPrint :: Bool
-  , optDebug       :: Bool
+  , optPrettyPrint :: Bool           -- ^ Pretty print output?
+  , optDebug       :: Bool           -- ^ For testing
   , optMode        :: Mode
   } deriving (Eq, Show)
 
-defaultOpts :: Options
-defaultOpts = Options
-  { optShowHelp    = False
-  , optShowVersion = False
-  , optInputPath   = Nothing
-  , optShrinkIndex = mempty
-  , optOutputPath  = Nothing
-  , optPrettyPrint = True
-  , optDebug       = False
-  , optMode        = ShowDescendant
-  }
 
-options :: [OptDescr (Options -> Either String Options)]
-options =
-  [ let munge opts = pure $ opts { optShowHelp = True }
-    in Option ['?'] ["help"] (NoArg munge)
-        "show usage"
-
-  , let munge opts = pure $ opts { optShowVersion = True }
-    in Option [] ["version"] (NoArg munge)
-        "show version information"
-
-  , let munge mPath opts = pure $ opts { optInputPath = mPath }
-    in Option [] ["input"] (OptArg munge "FILE")
-        "read from FILE (default is stdin)"
-
-  , let
-      munge d opts = do
-        ix <- parseShrinkIndexOption d
-        pure $ opts { optShrinkIndex = ix }
-    in
-      Option [] ["shrink-index"] (ReqArg munge "STRING")
-        "comma delimited list of natural numbers, e.g. '1,3,3,7' (default is the empty list)"
-      
-  , let munge mPath opts = pure $ opts { optOutputPath = mPath }
-    in Option [] ["output"] (OptArg munge "FILE")
-        "write to FILE (default is stdout)"
-
-  , let munge opts = pure $ opts { optPrettyPrint = False }
-    in Option [] ["notpretty"] (NoArg munge)
-        "do not pretty print output"
-        
-  , let munge opts = pure $ opts { optDebug = True }
-    in Option [] ["debug"] (NoArg munge)
-        "Use trees of integers instead of test scripts"
-  ]
 
 -- Very permissive; given a string, interpret any maximal contiguous
 -- substring of digits as a number and all other characters as delimiters.
@@ -185,19 +150,14 @@ options =
 --   parseShrinkIndex "o1w2o34w"  == Right path [1,2,34]
 parseShrinkIndexOption :: String -> Either String ShrinkIndex
 parseShrinkIndexOption =
-  fmap path . sequenceA . fmap readEither . filter (all isDigit) . groupBy bothDigits
-  where bothDigits u v = isDigit u && isDigit v
+  fmap path . traverse readEither . filter (all isDigit) . groupBy bothDigits
+  where bothDigits = on (&&) isDigit
 
 
 
 -- Like putStrLn, but for stderr.
 errPutStrLn :: String -> IO ()
 errPutStrLn msg = hPutStr stderr msg >> hPutChar stderr '\n'
-
-showUsage :: IO ()
-showUsage = do
-  let header = "USAGE: " <> nameString <> " [--OPTION...] [PATH]"
-  putStrLn $ usageInfo header options
 
 showVersion :: IO ()
 showVersion = putStrLn versionString
