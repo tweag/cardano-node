b5557df16827116e34c68a2e7ebb5d054716a103
Nathan Bloomfield <nathan.bloomfield@tweag.io>
2025-12-16 16:15:05 -0600

    Refactor main

--- cardano-node/app/conformance-test-viewer.hs
+++ cardano-node/app/conformance-test-viewer.hs
@@ -3,13 +3,19 @@
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Main (main) where
 
 import           Prelude hiding (lookup)
 
-import           Control.Monad (when)
-import           Data.Aeson (eitherDecode, encode, FromJSON, ToJSON)
+import           Control.Monad.Except (ExceptT(), runExceptT, throwError)
+import           Control.Monad.IO.Class (liftIO)
+import           Control.Monad.Reader (ReaderT(), asks, runReaderT)
+import           Data.Aeson (eitherDecode, encode, FromJSON(..), ToJSON(..))
 import           Data.Aeson.Encode.Pretty (encodePretty)
 import qualified Data.ByteString.Lazy as BS
 import           Data.Char (isDigit)
@@ -18,94 +24,51 @@ import           Data.Proxy
 import           Data.List (groupBy)
 import qualified ExitCodes as Exit
 import           Options.Applicative
+import           Ouroboros.Consensus.Byron.Ledger.Block
 import           ShrinkIndex
 import           System.Environment (getArgs)
 import           System.Exit
 import           System.IO (hPutStr, hPutStrLn, hPutChar, stderr)
-import           Test.QuickCheck (Arbitrary)
+import           Test.Consensus.OrphanInstances ()
+import           Test.Consensus.PointSchedule (GenesisTest, PointSchedule)
+import           Test.QuickCheck (Arbitrary(..), Gen)
 import           Text.Read (readEither)
 
-
-
-main :: IO ()
-main = do
-  opts <- getArgs >>= getOptions
-
-  when (optShowVersion opts) $
-    showVersion >> Exit.exitWithStatus Exit.Success
-  
-  case optDebug opts of
-    True  -> analyzeTreeOf (Proxy :: Proxy Int) opts
-    False -> analyzeTreeOf (Proxy :: Proxy Int) opts -- For now just a dummy implementation
+nameString, versionString :: String
+nameString    = "conformance-test-viewer"
+versionString = "v0.0"
 
 
 
--- Analyze the shrink tree of a value of any type that
--- implements `Arbitrary`, `FromJSON`, and `ToJSON`.
-analyzeTreeOf
-  :: forall a. (ToJSON a, FromJSON a, Arbitrary a)
-  => Proxy a -> Options -> IO ()
-analyzeTreeOf _ opts = do
-  input <- case optInputPath opts of
-    Nothing -> BS.getContents
-    Just iPath -> BS.readFile iPath
-    
-  testCase <- case eitherDecode input of
-    Right ok -> pure (ok :: a)
-    Left err -> do
-      errPutStrLn $ "Input decoding error: " <> err
-      exitFailure
-  
-  let tree = arbitraryShrinkTree testCase
-
-  case optMode opts of
-    ShowDescendant -> do
-      let result = lookup (optShrinkIndex opts) tree
-      descendant <- case result of
-        Nothing -> do
-          errPutStrLn $ "Descendant does not exist. :("
-          exitFailure
-        Just ok -> pure ok
-  
-      let bytes = if optPrettyPrint opts
-            then encodePretty descendant
-            else encode descendant
-        
-      case optOutputPath opts of
-        Nothing -> BS.putStr bytes >> putStrLn ""
-        Just oPath -> BS.writeFile oPath bytes
-
-
-
-getOptions :: [String] -> IO Options
-getOptions args = do
-  case execParserPure defaultPrefs options args of
-    Success opts -> pure opts
-    Failure failure -> do
-      let (msg, _) = renderFailure failure nameString
-      hPutStrLn stderr msg
-      Exit.exitWithStatus Exit.BadUsage
-    CompletionInvoked compl -> do
-      msg <- execCompletion compl nameString
-      putStr msg
-      Exit.exitWithStatus Exit.Success
-
-
+data Options = Options
+  { optInputPath    :: Maybe FilePath -- ^ Where to read input (default stdin)
+  , optShrinkIndex  :: ShrinkIndex    -- ^ Which shrink of the input to analyze
+  , optOutputPath   :: Maybe FilePath -- ^ Where to write output (default stdout)
+  , optPrettyPrint  :: Bool           -- ^ Pretty print output? (default true)
+  , optTestCaseType :: TestCaseType   -- ^ For testing; specify a test case type
+  , optMode         :: Mode
+  } deriving (Eq, Show)
 
 data Mode
   = ShowDescendant
   deriving (Eq, Show)
 
+data TestCaseType
+  = IntTC | StringTC | GenesisTestTC
+  deriving (Eq, Show)
+
 options :: ParserInfo Options
-options = info (infoOption "INFO" (long "help" <> short '?') <*> optionParser) $ mconcat
-  [ fullDesc
-  ]
+options = info
+  (optionParser <**> helper <**> simpleVersioner versionString) $
+  mconcat
+    [ fullDesc
+    , progDesc "A tool for viewing shrunken test cases."
+    , header "viewer - a conformance test viewer"
+    ]
 
 optionParser :: Parser Options
 optionParser = Options
-  <$> flag False True
-    (long "version")
-  <*> (option auto
+  <$> (option (Just <$> str)
     (long "input" <> mconcat
         [ metavar "FILE_PATH"
         , value Nothing
@@ -118,27 +81,114 @@ optionParser = Options
         , value (path [])
         , help "An index pointing to a shrunken test case"
         ]))
-  <*> (option auto
+  <*> (option (Just <$> str)
     (long "output" <> mconcat
         [ metavar "FILE_PATH"
         , value Nothing
         , help "File path writing output"
         ]))
   <*> flag True False
-    (long "notpretty")
-  <*> flag False True
-    (long "debug")
+    (long "notpretty" <> mconcat
+        [ help "Do not pretty print output"
+        ])
+  <*> (option (eitherReader parseTestCaseType)
+    (long "type" <> mconcat
+      [ short 't'
+      , value GenesisTestTC
+      , metavar "TYPE"
+      , help "Which type of test case to parse"
+      ]))
   <*> pure ShowDescendant
 
-data Options = Options
-  { optShowVersion :: Bool           -- ^ Show version info?
-  , optInputPath   :: Maybe FilePath
-  , optShrinkIndex :: ShrinkIndex
-  , optOutputPath  :: Maybe FilePath
-  , optPrettyPrint :: Bool           -- ^ Pretty print output?
-  , optDebug       :: Bool           -- ^ For testing
-  , optMode        :: Mode
-  } deriving (Eq, Show)
+
+
+-- Helper monad for the main function, providing the
+-- options environment and early exit.
+type ViewerM a = ReaderT Options (ExceptT String IO) a
+
+runViewerM :: Options -> ViewerM a -> IO (Either String a)
+runViewerM opts = runExceptT . flip runReaderT opts
+
+runWithHandler :: ViewerM a -> (String -> IO a) -> Options -> IO a
+runWithHandler act handle opts =
+  runViewerM opts act >>= either handle pure
+
+
+main :: IO ()
+main = getArgs >>= getOptions
+  >>= runWithHandler (getInputTestCase >>= analyzeShrinkTree >>= writeOutputTestCase)
+        (\err -> hPutStr stderr (err <> "\n") >> Exit.exitWithStatus Exit.InternalError)
+
+
+
+-- | This utility is parametric over the test case type, but at run time
+-- we have to instantiate the input at a specific type. To achieve this
+-- we hide the concrete type `a` behind an existential.
+data ViewableTestCase where
+  TestCase :: (FromJSON a, ToJSON a, Arbitrary a) => a -> ViewableTestCase
+
+
+
+-- | Parse command line options
+getOptions :: [String] -> IO Options
+getOptions args = do
+  case execParserPure defaultPrefs options args of
+    Success opts -> pure opts
+    Failure failure -> do
+      let (msg, _) = renderFailure failure nameString
+      hPutStrLn stderr msg
+      Exit.exitWithStatus Exit.BadUsage
+    CompletionInvoked compl -> do
+      execCompletion compl nameString >>= putStr
+      Exit.exitWithStatus Exit.Success
+
+-- | Determine from the program options what type the input test
+-- case should be instantiated at, and then read it.
+getInputTestCase
+  :: ViewerM ViewableTestCase
+getInputTestCase = do
+  testCaseType <- asks optTestCaseType
+  case testCaseType of
+    IntTC         -> readInputTestCase @Int
+    StringTC      -> readInputTestCase @String
+    GenesisTestTC -> readInputTestCase @(GenesisTest ByronBlock (PointSchedule ByronBlock))
+
+-- | Read and parse a JSON-encoded test case either
+-- from a file or from stdin.
+readInputTestCase
+  :: forall a. (ToJSON a, FromJSON a, Arbitrary a)
+  => ViewerM ViewableTestCase
+readInputTestCase = do
+  input <- asks optInputPath
+    >>= (liftIO . maybe BS.getContents BS.readFile)
+
+  TestCase <$> case eitherDecode input of
+    Right ok -> pure (ok :: a)
+    Left err -> throwError $ "Input decoding error: " <> err
+
+-- | Analyze the shrink tree of a value of any type that
+-- implements `Arbitrary`, `FromJSON`, and `ToJSON`;
+-- returns the resulting test case.
+analyzeShrinkTree :: ViewableTestCase -> ViewerM ViewableTestCase
+analyzeShrinkTree (TestCase testCase) = do
+  mode <- asks optMode
+  TestCase <$> case mode of
+    ShowDescendant -> do
+      shrinkIndex <- asks optShrinkIndex
+      maybe (throwError "Descendant does not exist. :(") pure $
+        lookup shrinkIndex $ arbitraryShrinkTree testCase
+
+writeOutputTestCase :: ViewableTestCase -> ViewerM ()
+writeOutputTestCase (TestCase testCase) = do
+  doPrettyPrint <- asks optPrettyPrint
+  let bytes = case doPrettyPrint of
+        True  -> encodePretty testCase
+        False -> encode testCase
+
+  outputPath <- asks optOutputPath
+  liftIO $ case outputPath of
+    Nothing -> BS.putStr bytes >> putStrLn ""
+    Just oPath -> BS.writeFile oPath bytes
 
 
 
@@ -153,17 +203,9 @@ parseShrinkIndexOption =
   fmap path . traverse readEither . filter (all isDigit) . groupBy bothDigits
   where bothDigits = on (&&) isDigit
 
-
-
--- Like putStrLn, but for stderr.
-errPutStrLn :: String -> IO ()
-errPutStrLn msg = hPutStr stderr msg >> hPutChar stderr '\n'
-
-showVersion :: IO ()
-showVersion = putStrLn versionString
-
-nameString :: String
-nameString = "conformance-test-viewer"
-
-versionString :: String
-versionString = nameString <> "-0.0"
+parseTestCaseType :: String -> Either String TestCaseType
+parseTestCaseType str = case str of
+  "int"     -> Right IntTC
+  "string"  -> Right StringTC
+  "genesis" -> Right GenesisTestTC
+  _ -> Left $ "Unrecognized test case type \"" <> str <> "\""
