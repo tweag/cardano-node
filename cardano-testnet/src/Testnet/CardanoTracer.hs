9142375be7be18232e0fe719e76cb98906f8d901
Sandy Maguire <sandy.maguire@tweag.io>
2026-09-16 12:03:34 -0700

    Generate cardano-tracer config

--- cardano-testnet/src/Testnet/CardanoTracer.hs
+++ cardano-testnet/src/Testnet/CardanoTracer.hs
@@ -9,12 +9,14 @@ module Testnet.CardanoTracer
   , withCardanoTracer
   ) where
 
-import           Cardano.Api
 
-import           Cardano.Testnet
+import           Testnet.Filepath
+import           Cardano.Tracer.Configuration
 
 import           Prelude
 
+import           Data.Aeson (encodeFile)
+import           Data.List.NonEmpty (NonEmpty(..))
 import qualified System.IO as IO
 import qualified System.Process as IO
 
@@ -22,23 +24,43 @@ import           Testnet.Process.Run (procCardanoTracer)
 
 import qualified Hedgehog as H
 import           Hedgehog.Extras (Integration)
-import           Hedgehog.Extras.Stock (Sprocket (..))
 import qualified Hedgehog.Extras.Stock.IO.Network.Socket as IO
-import qualified Hedgehog.Extras.Stock.IO.Network.Sprocket as IO
 import qualified Hedgehog.Extras.Test.Base as H
 import qualified Hedgehog.Extras.Test.Process as H
 
 data CardanoTracerConf = CardanoTracerConf
-  { testnetMagic  :: Int
+  { tempAbsPath   :: FilePath
+  , testnetMagic :: Int
+  , logPath :: FilePath
+  , logFormat :: LogFormat
   } deriving (Eq, Show)
 
-withCardanoTracer :: CardanoTracerConf -> [String] -> (String -> Integration ()) -> Integration ()
-withCardanoTracer
-    CardanoTracerConf
-      { testnetMagic
-      } args f = do
+mkConfig :: CardanoTracerConf -> Int -> FilePath -> TracerConfig
+mkConfig CardanoTracerConf { testnetMagic, logPath, logFormat } port socketPath = TracerConfig
+  { networkMagic = fromIntegral testnetMagic
+  , network = AcceptAt $ LocalPipe socketPath
+  , loRequestNum = Nothing
+  , ekgRequestFreq = Nothing
+  , hasEKG = Nothing
+  , hasPrometheus = Just $ Endpoint "127.0.0.1" port $ Just False
+  , hasTimeseries = Nothing
+  , tlsCertificate = Nothing
+  , hasForwarding = Nothing
+  , logging = LoggingParams logPath FileMode logFormat :| []
+  , rotation = Nothing
+  , verbosity = Nothing
+  , metricsNoSuffix = Nothing
+  , metricsHelp = Nothing
+  , resourceFreq = Nothing
+  , ekgRequestFull = Nothing
+  , prometheusLabels = Nothing
+  }
+
+withCardanoTracer :: CardanoTracerConf -> (FilePath -> Integration ()) -> Integration ()
+withCardanoTracer conf@CardanoTracerConf{tempAbsPath} k = do
   let logDir = makeLogDir $ TmpAbsolutePath tempAbsPath
       tempBaseAbsPath = makeTmpBaseAbsPath $ TmpAbsolutePath tempAbsPath
+      socketPath = undefined
 
   nodeStdoutFile <- H.noteTempFile logDir "cardano-tracer.stdout.log"
   nodeStderrFile <- H.noteTempFile logDir "cardano-tracer.stderr.log"
@@ -46,11 +68,13 @@ withCardanoTracer
   hNodeStdout <- H.evalIO $ IO.openFile nodeStdoutFile IO.WriteMode
   hNodeStderr <- H.evalIO $ IO.openFile nodeStderrFile IO.WriteMode
 
-  [prometheusPort] <- H.evalIO $ maybe (IO.allocateRandomPorts 1) (pure . (:[])) maybePort
+  [prometheusPort] <- H.evalIO $ IO.allocateRandomPorts 1
+  let configFile = undefined
+  H.evalIO $ encodeFile configFile $ mkConfig conf prometheusPort socketPath
 
   cp <- procCardanoTracer $
-    [ "--config", unFile configPath
-    ] <> args
+    [ "--config", configFile
+    ]
 
   (_, _, _, hProcess, _) <- H.createProcess $ cp
     { IO.std_in = IO.CreatePipe
@@ -62,7 +86,7 @@ withCardanoTracer
   H.onFailure $ H.evalIO $ IO.terminateProcess hProcess
   H.noteShow_ =<< H.getPid hProcess
 
-  let uriBase = "http://localhost:" <> show prometheusPort
-  f uriBase
+  H.evalIO $ putStrLn $ "Prometheus is running at http://localhost:" <> show prometheusPort
+  k socketPath
 
   H.evalIO $ IO.terminateProcess hProcess
